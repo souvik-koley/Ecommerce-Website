e527708ed939bb792fe9a22ee4dba5e108868c8f
souvikkoley9674 <113795422+souvikkoley9674@users.noreply.github.com>
2023-07-20 14:08:21 +0530

    Second commit

--- main.js
+++ main.js
@@ -37,6 +37,7 @@ function ready() {
         let button = addCart[i];
         button.addEventListener("click", addCartClicked);
     }
+    loadCartItems();
 }
 
 //Remove Cart Item
@@ -44,6 +45,7 @@ removeCartItem = (event) => {
     let buttonClicked = event.target;
     buttonClicked.parentElement.remove();
     updatetotal();
+    saveCartItems();
 }
 //Quantity Change
 quantityChanged = (event) => {
@@ -52,18 +54,19 @@ quantityChanged = (event) => {
         input.value = 1;
     }
     updatetotal();
+    saveCartItems();
 }
 
 //Add Cart Function
 addCartClicked = (event) => {
     let button = event.target;
     let shopProducts = button.parentElement;
-    // console.log(shopProducts.getElementsByClassName("product-img")[0].src);
     let title = shopProducts.getElementsByClassName("product-title")[0].innerText;
     let price = shopProducts.getElementsByClassName("price")[0].innerText;
     let productImg = shopProducts.getElementsByClassName("product-img")[0].src;
     addProductToCart(title, price, productImg);
     updatetotal();
+    saveCartItems();
 }
 
 addProductToCart = (title, price, productImg) => {
@@ -71,7 +74,6 @@ addProductToCart = (title, price, productImg) => {
     cartShopBox.classList.add("cart-box");
     let cartItems = document.getElementsByClassName('cart-content')[0];
     let cartItemsNames = cartItems.getElementsByClassName('cart-product-title');
-    // console.log(cartItems.getElementsByClassName('cart-product-title')[0].innerText);
     for (let i = 0; i < cartItemsNames.length; i++) {
         if (cartItemsNames[i].innerText == title) {
             alert("You have already added this item to cart");
@@ -96,6 +98,7 @@ addProductToCart = (title, price, productImg) => {
     cartItems.append(cartShopBox);
     cartShopBox.getElementsByClassName('cart-remove')[0].addEventListener('click', removeCartItem);
     cartShopBox.getElementsByClassName('cart-quantity')[0].addEventListener('change', quantityChanged);
+    // saveCartItems();
 }
 
 //update total
@@ -115,8 +118,54 @@ function updatetotal() {
     }
     total = Math.round(total * 100) / 100;
     document.getElementsByClassName("total-price")[0].innerText = "$" + total;
+    //Save Total To LocalStorage
+    localStorage.setItem('cartTotal', total);
 }
 
 
+//Keep Item in cart when page refresh with localstorage
+saveCartItems = () => {
+    let cartContent = document.getElementsByClassName("cart-content")[0];
+    let cartBoxes = cartContent.getElementsByClassName("cart-box");
+    let cartItems = [];
+    for (let i = 0; i < cartBoxes.length; i++) {
+        let cartBox = cartBoxes[i];
+        let titleElement = cartBox.getElementsByClassName('cart-product-title')[0];
+        let priceElement = cartBox.getElementsByClassName('cart-price')[0];
+        let quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+        let productImg = cartBox.getElementsByClassName('cart-img')[0].src;
+
+        let item = {
+            title: titleElement.innerText,
+            price: priceElement.innerText,
+            quantity: quantityElement.value,
+            productImg: productImg
+        };
+        cartItems.push(item);
+    }
+    console.log(cartItems);
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+}
 
 
+//Loads In Cart
+loadCartItems = () => {
+    let cartItems = localStorage.getItem('cartItems');
+    if (cartItems) {
+        cartItems = JSON.parse(cartItems);
+
+        for (let i = 0; i < cartItems.length; i++) {
+            const item = cartItems[i];
+            addProductToCart(item.title, item.price, item.productImg);
+            // console.log(item.title);
+            let cartBoxes = document.getElementsByClassName("cart-box");
+            let cartBox = cartBoxes[cartBoxes.length - 1];
+            let quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+            quantityElement.value = item.quantity;
+        }
+    }
+    let cartTotal = localStorage.getItem('cartTotal');
+    if (cartTotal) {
+        document.getElementsByClassName('total-price')[0].innerText = "$" + cartTotal;
+    }
+}
